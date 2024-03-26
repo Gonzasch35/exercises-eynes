7c194ff7b7bcffb5d60e14a8fe7a8a426109a577
Gonzalo Schmidt <gonza.sch35@gmail.com>
2024-03-26 13:13:04 -0300

    circle_second

--- circle/circle.py
+++ circle/circle.py
@@ -10,4 +10,14 @@ class Circle():
     def get_radius(self):
         return self.radius
 
+    def set_radius(self, radius):
+        if radius <= 0:
+            raise ValueError('El radio debe ser mayor a cero.')
+        else:
+            self.radius = radius
     
+    def get_area(self):
+        return math.pi * self.radius ** 2
+    
+    def get_perimeter(self):
+        return 2 * math.pi * self.radius

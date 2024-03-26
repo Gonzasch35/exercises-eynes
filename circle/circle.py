c026357a2726fd6458f42fdb451af80d8075fbe2
Gonzalo Schmidt <gonza.sch35@gmail.com>
2024-03-26 13:16:03 -0300

    circle_third

--- circle/circle.py
+++ circle/circle.py
@@ -21,3 +21,11 @@ class Circle():
     
     def get_perimeter(self):
         return 2 * math.pi * self.radius
+    
+    def __mul__(self, n):
+        if n <= 0: 
+            raise ValueError('El numero debe ser mayor a cero.')
+        return Circle(self.radius * n)
+    
+    def __str__(self):
+        return f'El area del circulo de radio {self.radius} es {self.get_area()} y el perimetro es {self.get_perimeter()}'

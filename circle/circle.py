5ccd5606cf230dd1a601ed18c4b2d6fa2ec7d2bf
Gonzalo Schmidt <gonza.sch35@gmail.com>
2024-03-26 13:08:29 -0300

    circle_first

--- circle/circle.py
+++ circle/circle.py
@@ -1,4 +1,13 @@
 import math
 
 class Circle():
-    pass
+
+    def __init__(self, radius):
+        if radius <= 0:
+            raise ValueError('El radio debe ser mayor a cero.')
+        self.radius = radius
+
+    def get_radius(self):
+        return self.radius
+
+    
